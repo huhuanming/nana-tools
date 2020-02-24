ca2fed7ac14d2e65863df9423d075a64b51b1110
huhuanming <huhuanming@meituan.com>
2020-02-25 03:57:43 +0800

    Update index.js

--- replace-in/index.js
+++ replace-in/index.js
@@ -4,15 +4,17 @@ const xlsx = require("node-xlsx").default;
 // const sourceFilePath = process.argv.pop();
 // const folderPath = process.argv.pop();
 
-const sourceFilePath = '/Users/huanminghu/Library/Containers/com.tencent.xinWeChat/Data/Library/Application\ Support/com.tencent.xinWeChat/2.0b4.0.9/76baac86150b63d88137e22b38c746f8/Message/MessageTemp/b76931d9ba7ea39622c94275c86872c1/File/表格-新/推月聚流-关联医院.xlsx';
-const folderPath = '/Users/huanminghu/Library/Containers/com.tencent.xinWeChat/Data/Library/Application\ Support/com.tencent.xinWeChat/2.0b4.0.9/76baac86150b63d88137e22b38c746f8/Message/MessageTemp/b76931d9ba7ea39622c94275c86872c1/File/表格-新/未命名文件夹';
+const sourceFilePath = '/Users/huanminghu/Library/Containers/com.tencent.xinWeChat/Data/Library/Application\ Support/com.tencent.xinWeChat/2.0b4.0.9/76baac86150b63d88137e22b38c746f8/Message/MessageTemp/b76931d9ba7ea39622c94275c86872c1/File/过滤替换公司名/推月聚流-关联医院.xlsx';
+const folderPath = '/Users/huanminghu/Library/Containers/com.tencent.xinWeChat/Data/Library/Application\ Support/com.tencent.xinWeChat/2.0b4.0.9/76baac86150b63d88137e22b38c746f8/Message/MessageTemp/b76931d9ba7ea39622c94275c86872c1/File/过滤替换公司名/excel';
 
 const workSheets = xlsx.parse(sourceFilePath);
 const nameMaps = {}
 
 workSheets[0].data.forEach((line, index) => {
     if (index > 1 && line[3]) {
-        nameMaps[(line[3].trim())] = line[2].trim()
+        if ((line[3].trim()) !== line[2].trim()) {
+            nameMaps[(line[3].trim())] = line[2].trim()
+        }
     }
 })
 
@@ -51,7 +53,7 @@ files.forEach((fileName) => {
                         ])
                     }
                 }
-                sheets[0].data[index][6] = "微博"
+                sheets[0].data[index][7] = "微博"
             }
         })
 
@@ -60,4 +62,7 @@ files.forEach((fileName) => {
         fs.writeFileSync(`${names.join('')}-replace-name.xlsx`, xlsx.build(sheets), "binary");
     }
 })
-fs.writeFileSync(`${folderPath}/no_found_names.xlsx`,  xlsx.build(logs), { encoding: 'buffer' })
+
+if (logs[0].data.length > 0) {
+    fs.writeFileSync(`${folderPath}/no_found_names.xlsx`,  xlsx.build(logs), { encoding: 'buffer' })
+}
