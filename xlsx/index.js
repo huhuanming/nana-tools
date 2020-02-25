92278bb65b6890792b6a46f61dfa68778848fecc
huhuanming <huhuanming@meituan.com>
2020-02-25 15:19:22 +0800

    修复空行判断 bug

--- xlsx/index.js
+++ xlsx/index.js
@@ -31,19 +31,21 @@ const splitExcel = path => {
         "录单时间"
       ]);
     } else {
-      if (line[2] && ~line[2].trim().indexOf("\n")) {
+      if (line[2] && ~line[2].indexOf("\n")) {
         line[2].split("\n").forEach(text => {
           if (text !== "") {
+            text = text.replace(/；/g, ';')
+            text = text.replace(/：/g, ':')
             const splits = text.split(";");
-            line[2] = splits[0];
+            line[2] = splits[0].trim();
             splitSheets[0].data.push([...line]);
             splitSheets[0].data[splitSheets[0].data.length - 1].splice(
               3,
               0,
-              splits[1].trim().split("：")[1],
-              splits[2].trim().split("：")[1],
-              splits[3].trim().split("：")[1],
-              splits[4].trim().split("：")[1]
+              splits[1].trim().split(":")[1],
+              splits[2].trim().split(":")[1],
+              splits[3].trim().split(":")[1],
+              splits[4].trim().split(":")[1]
             );
           }
         });
@@ -67,7 +69,8 @@ const splitExcel = path => {
   fs.writeFileSync(`${names.join('')}-split.xlsx`, buffer, "binary");
 };
 
-const folderPath = process.argv.pop();
+// const folderPath = process.argv.pop();
+const folderPath = '/Users/huanminghu/Library/Containers/com.tencent.xinWeChat/Data/Library/Application\ Support/com.tencent.xinWeChat/2.0b4.0.9/76baac86150b63d88137e22b38c746f8/Message/MessageTemp/b76931d9ba7ea39622c94275c86872c1/File/表格'
 
 const files = fs.readdirSync(folderPath)
 
